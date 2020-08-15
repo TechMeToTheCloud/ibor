9ca5b368b96a4380bde3fd7457e0a24f24d6f51c
gwadadakila <tiwesley1985@gmail.com>
2020-08-15 17:44:12 -0400

    added notebook and .net(for sending messages to event hubs)

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿namespace MyNamespace
+{
+    namespace SampleSender
+    {
+        using System;
+        using System.IO;
+        using System.Text;
+        using System.Threading.Tasks;
+        using Microsoft.Azure.EventHubs;
+
+        public class Program
+        {
+            private static EventHubClient eventHubClient;
+
+
+            private const string EventHubConnectionString = "Replace with your connection string key";
+            private const string EventHubName = "ibor";
+
+            public static void Main(string[] args)
+            {
+                MainAsync(args).GetAwaiter().GetResult();
+            }
+
+            private static async Task MainAsync(string[] args)
+            {
+                // Creates an EventHubsConnectionStringBuilder object from the connection string, and sets the EntityPath.
+                // Typically, the connection string should have the entity path in it, but for the sake of this simple scenario
+                // we are using the connection string from the namespace.
+                var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+                {
+                    EntityPath = EventHubName
+                };
+
+                eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+                await SendMessagesToEventHub();
+
+                await eventHubClient.CloseAsync();
+
+                Console.WriteLine("Press ENTER to exit.");
+                Console.ReadLine();
+            }
+
+
+            private static async Task SendMessagesToEventHub()
+            {
+                try
+                {
+                    var message = File.ReadAllText(@"replace with the path of your file");
+                    Console.WriteLine($"Sending message: {message}");
+                    await eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
+                }
+                catch (Exception exception)
+                {
+                    Console.WriteLine($"{DateTime.Now} > Exception: {exception.Message}");
+                }
+
+                Console.WriteLine("message sent.");
+            }
+        }
+    } 
+}
